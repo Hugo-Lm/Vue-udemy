88b9f4cdce3a6970e9c79589a1805c63db25b72f
Hugo Le Morvan <lm.hugo@hotmail.fr>
2021-01-27 15:49:29 +0100

    finished adding component

---
+++ 7-Vue-CLI-a-new-vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>My contact's list</h2>
+  <ul>
+    <friend-contact 
+      name="Hugo" 
+      phone-number="111 222 333" 
+      email-address="hugo@mail.fr">
+    </friend-contact>
+    <friend-contact
+      name="Julie"
+      phone-number="555 666 777"
+      email-address="julie@mail.pf">
+    </friend-contact>
+  </ul>
+</template>
+
+<script>
+// import FriendContact from './components/FriendContact.vue'
+  export default {
+  // components: { FriendContact },
+
+  }
+</script>
