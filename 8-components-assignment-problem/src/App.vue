8b5a5bacda45eca21e4bdde7e0afd5ab01abfcf5
Hugo Le Morvan <lm.hugo@hotmail.fr>
2021-01-28 11:26:03 +0100

    assignment components finished

---
+++ 8-components-assignment-problem/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>My infos : </h1>
+  <active-user 
+    :name="myName" 
+    :age="myAge">
+  </active-user>
+  <user-data
+    @new-data="updateData">
+  </user-data>
+</template>
+
+<script>
+// import UserData from './components/UserData.vue';
+// import ActiveUser from './components/ActiveUser.vue'
+export default {
+  // components: { UserData },
+  // components: { ActiveUser },
+  data() {
+    return {
+      myName: 'Hugo',
+      myAge: 25
+    }
+  },
+  methods: {
+    updateData(name, age) {
+      this.myName = name;
+      this.myAge = age;
+    }
+  }
+}
+</script>
---
+++ 8-components-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,26 @@
+<template>
+  <form>
+    <label for="name">New name:</label>
+    <input type="text" id="name" v-model="newName">
+    <label for="age">New age:</label>
+    <input type="text" id="age" v-model="newAge">
+    <input type="submit" @click="sendNewData">
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newName: '',
+      newAge: ''
+    }
+  },
+  methods: {
+    sendNewData(event) {
+      event.preventDefault();
+      this.$emit('new-data', this.newName, parseInt(this.newAge))
+    }
+  }
+}
+</script>
